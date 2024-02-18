0a39ecbdc7cae9fba6bf0e01e4a47ae14f110584
Ron <ronnachum13@gmail.com>
2024-02-18 01:03:26 -0500

    Semantic search. DONEZOOOO

--- frontend/components/logo.tsx
+++ frontend/components/logo.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import Image from 'next/image'; // Import the Image component
+import logo from "../public/synthesis.png";
 
 export function Logo() {
   return (
@@ -7,19 +9,8 @@ export function Logo() {
         className="relative z-20 flex items-center text-lg font-medium"
         href="/"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-          stroke="currentColor"
-          strokeWidth="2"
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          className="mr-2 h-6 w-6"
-        >
-          <circle cx="12" cy="12" r="10" />
-        </svg>
-        Synthesis
+        <Image src={logo} alt="Synthesis" width={32} height={32} className="mr-2" />
+        <h1>Synthesis</h1>
       </Link>
     </div>
   );
