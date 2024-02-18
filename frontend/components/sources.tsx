d9f035b4220d2cd6d85cdd0a50bac4f9aec04361
Ron <ronnachum13@gmail.com>
2024-02-18 11:46:19 -0500

    Clean up sources cards

--- frontend/components/sources.tsx
+++ frontend/components/sources.tsx
@@ -12,16 +12,19 @@ interface Article {
   
   export function Sources({ articles }: SourcesProps) {
     return (
-    <div style={{ display: 'flex', overflowX: 'auto', width: '50%', height: '20vh', scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
-        <div style={{ display: 'flex', gap: '10px'}}>
-            {articles.map((article) => (
-                <div key={article.id} style={{ width: '15vw', height: '15vh', padding: '1vh', borderRadius: '0.5vw', boxShadow: '0 2px 5px rgba(0,0,0,0.1'}}>
-                    <a href={article.url} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none'}}>
-                    <h4 style={{ pointerEvents: 'none' }}>{article.title} - {article.publisher.toUpperCase()}</h4>
-                    </a>
-                </div>
-            ))}
+    <>
+        <h2 className="text-xl mb-3"><strong>Selected Sources:</strong></h2>
+        <div style={{ display: 'flex', overflowX: 'auto', width: '50%', height: '20vh', scrollbarWidth: 'none', msOverflowStyle: 'none' }}>
+            <div style={{ display: 'flex', gap: '10px'}}>
+                {articles.map((article) => (
+                    <div key={article.id} className="border border-solid" style={{ width: '15vw', height: '15vh', padding: '1vh', borderRadius: '0.5vw', boxShadow: '0 5px 5px rgba(0,0,0,0.1)' }}>
+                        <a href={article.url} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none'}}>
+                            <h4 style={{ pointerEvents: 'none' }}><strong>{article.publisher.toUpperCase()}</strong><br /> {article.title}</h4>
+                        </a>
+                    </div>
+                ))}
+            </div>
         </div>
-      </div>
+      </>
     );
   }
